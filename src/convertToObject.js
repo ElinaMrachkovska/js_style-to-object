dbfd8783f1765b6138d1da5e07eada323d2ce3e9
Elina <elina@MacBook-Pro-Elina.local>
2025-09-18 23:44:46 +0300

    style-to-obj

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,33 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const result = {};
+
+  if (!sourceString || typeof sourceString !== 'string') {
+    return result
+  };
+
+  const declaration = sourceString.split(';');
+
+  for (let decl of declaration) {
+    decl = decl.trim();
+
+    if (!decl) {
+      continue;
+    }
+    
+    const firstColon = decl.indexOf(':');
+    if (firstColon === -1) continue;
+
+    const property = decl.slice(0, firstColon).trim();
+    const value = decl.slice(firstColon + 1).trim();
+
+    if (property) {
+      result[property]= value;
+    }
+  }
+
+return result;
 }
 
 module.exports = convertToObject;
